cf3a8bef50231c73809e63008814654b7c4d00e3
linuxing3 <linuxing3@qq.com>
2020-03-10 20:10:44 -0400

    opt

--- sh/core.sh
+++ sh/core.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y wget curl elinks
 
 echo "Install build tools"
 sudo apt-get install -y build-essential
-sudo apt-get install -y python-software-properties python3 python-pip g++ make
-sudo apt-get install -y libcairo2-dev libav-tools nfs-common portmap
+sudo apt-get install -y python3 python-pip g++ make
+sudo apt-get install -y libcairo2-dev nfs-common portmap
 
 sudo pip install ansible
--- sh/python.sh
+++ sh/python.sh
@@ -2,7 +2,17 @@ echo "==========================================================="
 echo "installing python3"
 echo "==========================================================="
 
-sudo apt install -y python3 python3-pip
-# using pyenv
+sudo apt-get install -y python3 python3-pip
+# Make sure to use python 3
+sudo rm /usr/bin/python
+sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo rm /usr/bin/pip
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+
+
 cd
-curl https://pyenv.run | bash
+# using ansible
+pip3 install --user ansible
+# using pipenv
+pip3 install --user pipenv
+echo "export PATH=$PATH:/home/vagrant/.local/bin" >> /home/vagrant/.bashrc
